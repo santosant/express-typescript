202928e3ecee795d20731a58e8d79ab33aee3319
Antonio Anderson <hiroto.anderson@gmail.com>
2022-05-06 00:15:14 -0300

    morgan middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express, { Application } from "express"
-
+import morgan from "morgan"
 const PORT = process.env.PORT || 3333
 
 const app: Application = express();
+
+app.use(express.json())
+app.use(morgan("tiny"))
+app.use(express.static("public"))
 
 app.get("/ping", async (req, res) => {
   res.send({
